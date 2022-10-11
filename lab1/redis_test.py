0c1d175bffac0bac5d14037b12f93dafc3ab039a
wibox <francesco.inf.pagano@gmail.com>
2022-10-11 19:07:44 +0200

    commented examples

--- lab1/redis_test.py
+++ lab1/redis_test.py
@@ -15,13 +15,13 @@ client = redis.Redis(
 
 print("Is connected: ", client.ping())
 
-#to insert a key-value pair
-client.set("first MSG", "Hello, World!")
-#to retrieve a value from key -> gets returned in bits -> .decode()
-print(client.get("first MSG").decode())
+# #to insert a key-value pair
+# client.set("first MSG", "Hello, World!")
+# #to retrieve a value from key -> gets returned in bits -> .decode()
+# print(client.get("first MSG").decode())
 
-#how to create a time series
-client.ts().create("temperature")
-timestamp_in_seconds = time.time()
-timestamp_in_ms = int(timestamp_in_seconds * 1000)
-client.ts().add("temperature", timestamp=timestamp_in_ms, value=25.6)
+# #how to create a time series
+# client.ts().create("temperature")
+# timestamp_in_seconds = time.time()
+# timestamp_in_ms = int(timestamp_in_seconds * 1000)
+# client.ts().add("temperature", timestamp=timestamp_in_ms, value=25.6)
